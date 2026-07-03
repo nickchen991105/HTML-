3093b3b2c56c9636226321cd75abf4e428687e28
nickchen991105 <151999018+nickchen991105@users.noreply.github.com>
2026-07-03 16:12:19 +0800

    Add files via upload

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let Nick = document.getElementById('tools');
     let output = document.getElementById('agg1')
     Nick.addEventListener('click', function(){
-        output.innerHTML = '不要亂點 >:'
+        output.innerHTML = '<h1 style="font-size: 100px; color: #FF2D2D" id="agg1">不要亂按!!</h1>'
     })
 })
 
